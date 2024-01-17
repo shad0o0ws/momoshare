beae3f976e2debf0b2d6048a97a5631e3a7c651d
shad0o0ws <141566595+shad0o0ws@users.noreply.github.com>
2024-01-17 19:24:40 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=20175679&pid=662f5e00a288c231a644e71024835ef0&tid=bf9f5f2ae4cdc6b5d8a3018835a18a13",
+    "https://www.maimemo.com/share/page?uid=20175679&pid=fd7d3e7267b739fe7845d45676917685&tid=31003dd8e67f250fcff580f81e63e0bf",
 
 
   ]
